564caefdc8136dbe476f8e8d11778d9b7d4d71d7
alajos <louis.faure@curie.fr>
2018-05-03 15:44:31 +0200

    add rst automatic conversion

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
 
 setup(name='elpigraph',
       version='0.91',
       description='Python implementation of Elpigraph',
+      long_description=read_md('README.md'),
       url='https://github.com/LouisFaure/ElPiGraph.P',
       author='Alexi Martin',
       author_email='',
