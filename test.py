3537ccc8d5b966a1c654b0509fec1b80d7b8178f
alajos <louis.faure@curie.fr>
2018-05-03 14:04:29 +0200

    cleaning test.py

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-from elpigraph import computeElasticPrincipalCurve
-from elpigraph import computeElasticPrincipalCircle
 from elpigraph import computeElasticPrincipalTree
 from elpigraph import PlotPG
 import numpy as np
@@ -7,9 +5,5 @@ import numpy as np
 tree_data = np.genfromtxt('elpigraph/data/tree_data.csv', delimiter=',')
 EPTree = computeElasticPrincipalTree(tree_data, 20)
 
-#curve_data = np.genfromtxt('elpigraph/data/curve_data.csv', delimiter=',')
-#EPCurve = computeElasticPrincipalCurve(curve_data, 10,nReps=10,ProbPoints=0.9)
-
-#print(curve_data)
 
 PlotPG(tree_data,EPTree)
